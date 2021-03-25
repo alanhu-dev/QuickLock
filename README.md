0e1ee1d5c18f0eecb02080df4827f45435da271c
Alan <alanhu@zeroplus.com.tw>
2021-03-25 11:15:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# ScreenLock - [Google Play](https://play.google.com/store/apps/details?id=com.superyao.screenlock)
+One-click to lock the screen.
+
+* The app used Accessibility Service
+* Only support Android 9 or higher
---
+++ app/src/main/java/com/superyao/quicklock/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.superyao.quicklock
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ScreenLock.start(this)
+        finish()
+    }
+}
