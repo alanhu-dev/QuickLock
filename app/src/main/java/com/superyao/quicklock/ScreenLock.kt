7d41799e70cf674f30d7209b3b47e68ad1e5c360
SUPERYAO <supersy943@gmail.com>
2021-10-01 01:20:40 +0800

    2 (1.0.1) * upgrade targetSdk to 31 * upgrade libs

--- app/src/main/java/com/superyao/quicklock/ScreenLock.kt
+++ app/src/main/java/com/superyao/quicklock/ScreenLock.kt
@@ -5,37 +5,26 @@ import android.content.Context
 import android.content.Intent
 import android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS
 import android.view.accessibility.AccessibilityEvent
-import android.widget.Toast
 
 class ScreenLock : AccessibilityService() {
-
-    companion object {
-        fun start(context: Context) = context.startService(
-            Intent(context, ScreenLock::class.java)
-        )
-    }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {}
 
     override fun onInterrupt() {}
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        try {
-            if (!performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)) {
-                // failed, open the accessibility settings
-                startActivity(
-                    Intent(ACTION_ACCESSIBILITY_SETTINGS).apply {
-                        addFlags(Intent.FLAG_RECEIVER_FOREGROUND)
-                    }
-                )
-            }
-        } catch (e: Exception) {
-            Toast.makeText(
-                this,
-                "${getString(R.string.exception)}\n(${e.message})",
-                Toast.LENGTH_LONG
-            ).show()
+        if (!performGlobalAction(GLOBAL_ACTION_LOCK_SCREEN)) {
+            val accessibilitySettings = Intent(ACTION_ACCESSIBILITY_SETTINGS)
+                .addFlags(Intent.FLAG_RECEIVER_FOREGROUND)
+            startActivity(accessibilitySettings)
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, ScreenLock::class.java)
+            context.startService(intent)
+        }
     }
 }
