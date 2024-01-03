866f07cb65464937de88fb52997341eb10c4af5f
Alan Hu <alanhu.dev@gmail.com>
2024-01-03 14:08:34 +0800

    show "Accessibility Service API" message before use it

--- app/src/main/java/com/superyao/quicklock/MainActivity.kt
+++ app/src/main/java/com/superyao/quicklock/MainActivity.kt
@@ -2,11 +2,31 @@ package com.superyao.quicklock
 
 import android.app.Activity
 import android.os.Bundle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.superyao.quicklock.service.ScreenLockService
 
 class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (ScreenLockService.isEnabled(this)) {
+            lockScreen()
+            return
+        }
+        showGuide()
+    }
+
+    private fun showGuide() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(R.string.app_name)
+            .setMessage(R.string.accessibility_service_description)
+            .setPositiveButton(android.R.string.ok) { _, _ -> lockScreen() }
+            .setNegativeButton(android.R.string.cancel) { _, _ -> finish() }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun lockScreen() {
         ScreenLockService.start(this)
         finish()
     }
--- app/src/main/java/com/superyao/quicklock/service/ScreenLockService.kt
+++ app/src/main/java/com/superyao/quicklock/service/ScreenLockService.kt
@@ -3,6 +3,7 @@ package com.superyao.quicklock.service
 import android.accessibilityservice.AccessibilityService
 import android.content.Context
 import android.content.Intent
+import android.provider.Settings
 import android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS
 import android.view.accessibility.AccessibilityEvent
 
@@ -27,6 +28,16 @@ class ScreenLockService : AccessibilityService() {
     companion object {
         fun start(context: Context) {
             context.startService(Intent(context, ScreenLockService::class.java))
+        }
+
+        fun isEnabled(context: Context): Boolean {
+            val enabledServices = Settings.Secure.getString(
+                context.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES,
+            )
+            val packageName = context.packageName
+            val serviceCanonicalName = ScreenLockService::class.java.canonicalName
+            val service = "$packageName/$serviceCanonicalName"
+            return enabledServices?.contains(service) ?: false
         }
     }
 }
